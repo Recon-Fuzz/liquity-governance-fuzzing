4dbd1f41dcb4297483f986775def9834d26e62cc
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2024-10-13 11:37:26 -0300

    feat: governance scaffolding

--- test/recon/CryticToFoundry.sol
+++ test/recon/CryticToFoundry.sol
@@ -2,33 +2,27 @@
 // SPDX-License-Identifier: GPL-2.0
 pragma solidity ^0.8.0;
 
-import {FoundryAsserts} from "@chimera/FoundryAsserts.sol";
-import {Test} from "forge-std/Test.sol";
-
+import {Test, console2} from "forge-std/Test.sol";
 import {TargetFunctions} from "./TargetFunctions.sol";
-import {MaliciousInitiative} from "./mocks/MaliciousInitiative.sol";
-
+import {FoundryAsserts} from "@chimera/FoundryAsserts.sol";
 
 contract CryticToFoundry is Test, TargetFunctions, FoundryAsserts {
     function setUp() public {
         setup();
     }
 
-    function test_initiative_onRegisterInitiative() public {
-        initiative_onRegisterInitiative(5);
+    function test_depositLQTY_crytic() public {
+        governance_depositLQTY(1e18);
     }
 
-    function test_intiative_onUnregisterInitiative() public {
-        initiative_onRegisterInitiative(5);
-        intiative_onUnregisterInitiative(8);
+    function test_allocateLQTY_crytic() public {
+        governance_depositLQTY(1e18);
+
+        vm.warp(block.timestamp + 7 days);
+        governance_allocateLQTY_clamped_single_initiative(0, 5e17, 0);
     }
 
-    function test_initiative_onAfterAllocateLQTY() public {
-        maliciousInitiative.setRevertBehaviour(MaliciousInitiative.FunctionType.ALLOCATE, MaliciousInitiative.RevertType.OOG);
-        initiative_onAfterAllocateLQTY(5, address(0x101112), uint88(10), uint88(15));
-    }
-
-    function test_initiative_onClaimForInitiative() public {
-        initiative_onClaimForInitiative(5, 50);
+    function test_registerInitiative_crytic() public {
+        governance_registerInitiative(1);
     }
 }
--- test/recon/TargetFunctions.sol
+++ test/recon/TargetFunctions.sol
@@ -4,38 +4,110 @@ pragma solidity ^0.8.0;
 
 import {BaseTargetFunctions} from "@chimera/BaseTargetFunctions.sol";
 import {vm} from "@chimera/Hevm.sol";
-import {IInitiative} from "governance/interfaces/IInitiative.sol";
+import {IERC20Permit} from "openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Permit.sol";
+import {ILQTYStaking} from "governance/interfaces/ILQTYStaking.sol";
+import {IUserProxy} from "governance/interfaces/IUserProxy.sol";
+import {PermitParams} from "governance/utils/Types.sol";
 
+import {console2} from "forge-std/Test.sol";
 import {BeforeAfter} from "./BeforeAfter.sol";
 import {Properties} from "./Properties.sol";
-import {safeCallWithMinGas} from "./utils/SafeCallMinGas.sol";
+
+
 
 abstract contract TargetFunctions is BaseTargetFunctions, Properties, BeforeAfter {
 
-    function initiative_onRegisterInitiative(uint16 epoch) public {
-        bool callWithMinGas = safeCallWithMinGas(address(initiative), MIN_GAS_TO_HOOK, 0, abi.encodeCall(IInitiative.onRegisterInitiative, (epoch)));
+    // clamps to a single initiative to ensure coverage in case both haven't been registered yet
+    function governance_allocateLQTY_clamped_single_initiative(uint8 initiativesIndex, uint96 deltaLQTYVotes, uint96 deltaLQTYVetos) public {
+        // clamp using the user's staked balance
+        uint96 stakedAmount = IUserProxy(governance.deriveUserProxyAddress(user)).staked();
         
-        t(callWithMinGas, "call to onRegisterInitiative reverts with minimum gas");
-    }
-    
-    function intiative_onUnregisterInitiative(uint16 epoch) public {
-        bool callWithMinGas = safeCallWithMinGas(address(initiative), MIN_GAS_TO_HOOK, 0, abi.encodeCall(IInitiative.onUnregisterInitiative, (epoch)));
+        address[] memory initiatives = new address[](1);
+        initiatives[0] = _getDeployedInitiative(initiativesIndex);
+        int176[] memory deltaLQTYVotesArray = new int176[](1);
+        deltaLQTYVotesArray[0] = int176(uint176(deltaLQTYVotes % stakedAmount));
+        int176[] memory deltaLQTYVetosArray = new int176[](1);
+        deltaLQTYVetosArray[0] = int176(uint176(deltaLQTYVetos % stakedAmount));
         
-        t(callWithMinGas, "call to onUnregisterInitiative reverts with minimum gas");
-    }
-    
-    function initiative_onAfterAllocateLQTY(uint16 currentEpoch, address user, uint88 voteLQTY, uint88 vetoLQTY) public {
-        bool callWithMinGas = safeCallWithMinGas(address(initiative), MIN_GAS_TO_HOOK, 0, abi.encodeCall(IInitiative.onAfterAllocateLQTY, (currentEpoch, user, voteLQTY, vetoLQTY)));
-
-        t(callWithMinGas, "call to onAfterAllocateLQTY reverts with minimum gas");
+        governance.allocateLQTY(initiatives, deltaLQTYVotesArray, deltaLQTYVetosArray);
     }
 
-    function initiative_onClaimForInitiative(uint16 claimEpoch, uint256 bold) public {
-        bool callWithMinGas = safeCallWithMinGas(address(initiative), MIN_GAS_TO_HOOK, 0, abi.encodeCall(IInitiative.onClaimForInitiative, (claimEpoch, bold)));
+    // NOTE: this can be clamped to allocate to multiple initiatives at once
+    function governance_allocateLQTY(address[] calldata initiatives, int176[] calldata deltaLQTYVotes, int176[] calldata deltaLQTYVetos) public {
+        governance.allocateLQTY(initiatives, deltaLQTYVotes, deltaLQTYVetos);
+    }
+
+    function governance_claimForInitiative(address _initiative) public {
+        governance.claimForInitiative(_initiative);
+    }
+
+    function governance_claimFromStakingV1(address _rewardRecipient) public {
+        governance.claimFromStakingV1(_rewardRecipient);
+    }
+
+    function governance_deployUserProxy() public {
+        governance.deployUserProxy();
+    }
+
+    function governance_depositLQTY(uint88 _lqtyAmount) public {
+        governance.depositLQTY(_lqtyAmount);
+    }
+
+    function governance_depositLQTYViaPermit(uint88 _lqtyAmount) public {
+         // Get the current block timestamp for the deadline
+        uint256 deadline = block.timestamp + 1 hours;
+
+        // Create the permit message
+        bytes32 permitTypeHash = keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+        bytes32 domainSeparator = IERC20Permit(address(lqty)).DOMAIN_SEPARATOR();
         
-        t(callWithMinGas, "call to onClaimForInitiative reverts with minimum gas");
+        uint256 nonce = IERC20Permit(address(lqty)).nonces(user);
+        
+        bytes32 structHash = keccak256(abi.encode(
+            permitTypeHash,
+            user,
+            address(governance),
+            _lqtyAmount,
+            nonce,
+            deadline
+        ));
+
+        bytes32 digest = keccak256(abi.encodePacked(
+            "\x19\x01",
+            domainSeparator,
+            structHash
+        ));
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(user2Pk, digest);
+
+        PermitParams memory permitParams = PermitParams({
+            owner: user2,
+            spender: user,
+            value: _lqtyAmount,
+            deadline: deadline,
+            v: v,
+            r: r,
+            s: s
+        });
+
+        governance.depositLQTYViaPermit(_lqtyAmount, permitParams);
     }
 
+    function governance_registerInitiative(uint8 initiativeIndex) public {
+        address initiative = _getDeployedInitiative(initiativeIndex);
+        governance.registerInitiative(initiative);
+    }
 
+    function governance_snapshotVotesForInitiative(address _initiative) public {
+        governance.snapshotVotesForInitiative(_initiative);
+    }
 
+    function governance_unregisterInitiative(uint8 initiativeIndex) public {
+        address initiative = _getDeployedInitiative(initiativeIndex);
+        governance.unregisterInitiative(initiative);
+    }
+
+    function governance_withdrawLQTY(uint88 _lqtyAmount) public {
+        governance.withdrawLQTY(_lqtyAmount);
+    }
 }
