e71a839dbeb5d36346832b504c154b198cb4b0b3
Entreprenerd <alex@entreprenerd.xyz>
2024-10-25 10:39:30 +0200

    chore: log

--- test/recon/CryticToFoundry.sol
+++ test/recon/CryticToFoundry.sol
@@ -3,11 +3,16 @@ pragma solidity ^0.8.0;
 
 import {FoundryAsserts} from "@chimera/FoundryAsserts.sol";
 import {Test} from "forge-std/Test.sol";
+import {console} from "forge-std/console.sol";
 
 import {TargetFunctions} from "./TargetFunctions.sol";
 
 contract CryticToFoundry is Test, TargetFunctions, FoundryAsserts {
     function setUp() public {
         setup();
+    }
+
+    function test_basic() public {
+        console.log("test works");
     }
 }
